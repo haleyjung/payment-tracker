7418abd18abbe7344796d52ae3014875d726061e
haleyjung <haleyjung16@gmail.com>
2022-07-02 12:58:26 -0400

    Create components and corresponding test files

---
+++ src/components/Table/TableBody.test.js
@@ -0,0 +1,2 @@
+import { render, screen, fireEvent, within } from '@testing-library/react';
+import TableBody from './TableBody';
---
+++ src/components/Table/TableHead.test.js
@@ -0,0 +1,2 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import TableHead from './TableHead';
