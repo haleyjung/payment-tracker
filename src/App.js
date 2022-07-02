e227a31f8aef3e75094284bf5175eb4b09c01e18
haleyjung <haleyjung16@gmail.com>
2022-07-02 12:30:51 -0400

    init - Set up the app using Create React App - Replace the default React logo with Strategic Financial Solutions logo - Remove default contents, styles, and test - Install axios and prop-types - Create /server in the root directory and /components inside /src

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="app" data-testid="app">
+      <h1>
+        Strafts Coding Challenge
+        <span role="img" aria-label="Fire">
+          {' '}
+          🔥{' '}
+        </span>
+      </h1>
+      <div></div>
+    </div>
+  );
+}
